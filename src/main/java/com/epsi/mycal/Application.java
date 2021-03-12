308ceacd1247be41478913dabe44b2bf6af74b42
RENOU Florent <florent.renou01@gmail.fr>
2021-03-12 10:37:19 +0100

    reformat code

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -2,9 +2,9 @@ package com.epsi.mycal;
 
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World !");
+  public static void main(String[] args) {
+    System.out.println("Hello World !");
 
-	}
+  }
 
 }
